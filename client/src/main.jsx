ab0572e69ce46dfd1072949d97199455c2284cad
Danilenko <vdanilenko254@gmail.com>
2026-06-17 19:12:49 +0300

    added login, signup pages

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/auth/Login.jsx";
+import Signup from "./pages/auth/Signup.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
-)
+);
---
+++ client/src/pages/auth/Login.jsx
@@ -0,0 +1,51 @@
+import style from "./Auth.module.css";
+import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+const Login = () => {
+  const location = useLocation();
+
+  return (
+    <div className={style.wrapper}>
+      <div className={style.login}>
+        <h1>Habit Tracker</h1>
+        <p>Build better habits, one day at a time.</p>
+        <div className={style.buttons}>
+          <Link
+            className={location.pathname == "/login" ? style.active : ""}
+            to="/login"
+          >
+            Login
+          </Link>
+          <Link
+            className={location.pathname == "/signup" ? style.active : ""}
+            to="/signup"
+          >
+            Sign Up
+          </Link>
+        </div>
+
+        <form className={style.form}>
+          <h3>Welcome Back!</h3>
+          <p>Sign in to your account to continue.</p>
+          <div className={style.formGroup}>
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" />
+          </div>
+          <div className={style.formGroup}>
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" />
+          </div>
+          <div className={style.checkboxGroup}>
+            <input type="checkbox" id="rememberMe" />
+            <label htmlFor="rememberMe">Remember Me</label>
+          </div>
+          <button className={style.submitButton} type="submit">
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
