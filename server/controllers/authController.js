46eb6ac13441cea91bef9992aa47e7889b21e52e
Danilenko <vdanilenko254@gmail.com>
2026-06-19 12:18:29 +0300

    feat: added me controller function

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -60,4 +60,16 @@ const login = async (req, res) => {
   }
 };
 
-export { register, login };
+const me = async (req, res) => {
+  try {
+    const user = await User.findById(req.userId).select("-password");
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export { register, login, me };
